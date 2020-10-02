86cdaae9c8dc10e05515a7f47ea8ccb6b01bb071
juanoctavio10 <juanoctavio10>
2020-10-02 08:29:40 -0400

    Commit part 7

---
+++ post.php
@@ -0,0 +1,39 @@
+<html>
+
+<head> 
+    <title>Post Page</title> 
+</head> 
+
+<body>
+
+
+<header>
+    <h1>This is Task 7 Post</h1>
+</header>
+ 
+
+<nav>
+    <a href="/index.php">Index</a>
+    <a href="/post.php">Post</a>
+    <a href="/postsumission.php">PostSumission</a>
+</nav>
+
+<main>
+     <div>Autor Example</div> 
+     <div>10-01-2020</div>
+     <div>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+           culpa qui officia deserunt mollit anim id est laborum."
+    </div>
+</main>
+
+<footer>
+    <p>This will be the footer content<br>
+</footer>
+
+</body>
+
+</html>
---
+++ postsumission.php
@@ -0,0 +1,39 @@
+<html>
+
+<head> 
+    <title>PostSumission Page</title> 
+</head> 
+
+<body>
+
+
+<header>
+    <h1>This is Task 7 PostSumission</h1>
+</header>
+ 
+
+<nav>
+    <a href="/index.php">Index</a>
+    <a href="/post.php">Post</a>
+    <a href="/postsumission.php">PostSumission</a>
+</nav>
+
+<main>
+    <form action="/post.html">
+        <label for="fname">Title:</label><br>
+        <input type="text" id="title" name="title" value=""><br>
+        <label for="lname">Autor:</label><br>
+        <input type="text" id="autor" name="autor" value=""><br><br>
+        <label for="lname">Content:</label><br>
+        <textarea  id="content" name="content" value="" > </textarea> <br><br>
+        <input type="submit" value="Submit">
+      </form>
+</main>
+
+<footer>
+    <p>This will be the footer content<br>
+</footer>
+
+</body>
+
+</html>
