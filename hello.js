69f5083b9908b4061a2b28f256c67cdca173170d
juanoctavio10 <juanoctavio10>
2020-10-08 23:11:29 -0400

    Part 4 Week 5

--- hello.js
+++ hello.js
@@ -1,1 +1,31 @@
 alert('Hello');
+
+var firstName = "Jane";
+var lastName = "Doe";
+var age = 35;
+var married = true;
+var childrenNames = ["Alice","Bod"];
+var husband = { firstName: "John", lastName: "Doe", age:35 };
+
+var fullName = firstName +" "+ lastName;
+var dogYear = age/7;
+var isAdult = dogYear > 4;
+var isSeattled= isAdult && married;
+
+
+function getFullName(firstName, lastName){
+    return firstName+" "+ lastName;
+}
+
+var fullName= getFullName(firstName,lastName);
+var fullName= getFullName("Alice","Doc");
+
+if (isSeattled){
+    document.write(fullName+ "is settled");
+} else {
+    document.write(fullName+ "is not settled");
+}
+
+childrenNames.forEach(function(childName){
+    document.write("<li>"+ childName+"</li>")
+})
